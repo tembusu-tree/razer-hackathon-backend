bf3171e4e43e0ae09713a196962c4bc8016d6b06
Shelvia Hotama <shelvia.ho@gmail.com>
2020-05-16 18:47:38 +0800

    Rename

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import App from './app';
 import ClientsController from './clients/clients.controller';
-import HealthCheckController from './healthcheck/pongRouter';
+import HealthCheckController from './healthcheck/healthcheck.controller';
 
 const app = new App([new HealthCheckController(), new ClientsController()]);
 
