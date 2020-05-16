4b6156e7fb1c80a87b25a923dc4bcb096ce19004
Johandy Tantra <johandy@expcentre.com>
2020-05-17 01:02:19 +0800

    - Update Client Service

--- src/lib/mambu.services/ClientService.ts
+++ src/lib/mambu.services/ClientService.ts
@@ -5,49 +5,63 @@ import { MambuApiService } from './BaseService';
 const fieldNames = { acra: 'acraNumber', email: 'email' };
 
 export class ClientService extends MambuApiService {
-  private mapResponse(data: MambuClient) {
-    return {
-      firstName: data.firstName,
-      lastName: data.lastName,
-      email: data.customInformation.find(
-        (i) => i.customField.name === fieldNames.email,
-      ).value,
-      acra: data.customInformation.find(
-        (i) => i.customField.name === fieldNames.acra,
-      ).value,
-      mobile: data.mobilePhone1,
-    };
-  }
+  // private mapResponse(data: MambuClient) {
+  //   return {
+  //     firstName: data.firstName,
+  //     lastName: data.lastName,
+  //     email: data.idDocuments.find(
+  //       (i) => i.issuingAuthority === fieldNames.email,
+  //     ).value,
+  //     acra: data.customInformation.find(
+  //       (i) => i.customField.name === fieldNames.acra,
+  //     ).value,
+  //     mobile: data.mobilePhone1,
+  //   };
+  // }
 
-  async get(id: string): Promise<Client> {
+  async get(id: string): Promise<any> {
     const res = await this.apiGet('clients/' + id + '?fullDetails=true');
 
     const data: MambuClient = res.client;
 
-    return this.mapResponse(data);
+    return data;
   }
 
-  async create(client: Client): Promise<Client> {
+  async create(client: Client): Promise<any> {
     const body = {
-      firstName: client.firstName,
-      lastName: client.lastName,
-      assignedBranchKey: this.branchKey,
-      mobilePhone1: client.mobile,
-      customInformation: [
+      "client": {
+        "firstName": client.firstName,
+        "lastName": client.lastName,
+        "preferredLanguage": "ENGLISH",
+        "notes": "Enjoys playing RPG",
+        "assignedBranchKey": this.branchKey
+      },
+      "idDocuments": [
         {
-          value: client.acra,
-          customFieldID: 'acraNumber',
-        },
-        {
-          value: client.email,
-          customFieldID: 'email',
-        },
+          "identificationDocumentTemplateKey": "8a8e867271bd280c0171bf7e4ec71b01",
+          "issuingAuthority": "Immigration Authority of Singapore",
+          "documentType": "NRIC/Passport Number",
+          "validUntil": "2021-09-12",
+          "documentId": "S9812345A"
+        }
       ],
-    };
+      "addresses": [],
+      "customInformation": [
+        {
+          "value": "Singapore",
+          "customFieldID": "countryOfBirth"
+
+        },
+        {
+          "value": "{{$guid}}",
+          "customFieldID": "razerID"
+        }
+      ]
+    }
 
     const res = await this.apiPost('clients?fullDetails=true', body);
     const data: MambuClient = res.client;
 
-    return this.mapResponse(data);
+    return data;
   }
 }
